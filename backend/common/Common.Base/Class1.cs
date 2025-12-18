def032cf4f9c0d550834afee81a69f8b312891db
sadsophieFaceBoi <andrew_murphy1990@hotmail.com>
2025-12-18 20:51:28 +0000

    deleted everything starting again without ai..mostly

--- backend/common/Common.Base/Class1.cs
+++
@@ -1,6 +0,0 @@
-﻿namespace Common.Base;
-
-public class Class1
-{
-
-}
--- backend/functions/AuthFunctionApp/AuthProviderSettings.cs
+++
@@ -1,7 +0,0 @@
-public class AuthProviderSettings
-{
-    // Public OAuth client IDs / app IDs (not secrets). In Azure, set as App Settings.
-    public string? GoogleClientId { get; init; }
-    public string? MicrosoftClientId { get; init; }
-    public string? FacebookAppId { get; init; }
-}
